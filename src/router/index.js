f3c5caf3f2b9c44e34552d89d61f9b38b2495fbf
Agustin Tassano <agustin.tassano@gmail.com>
2022-12-12 16:31:10 +0100

    feat: create Auth component and path

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Dashboard from "../views/Dashboard.vue"
+import Auth from "../views/Auth.vue"
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +9,10 @@ const router = createRouter({
       path: "/", 
       component: Dashboard,
     },
+    {
+      path: "/",
+      component: Auth,
+    }
   ],
 })
 
